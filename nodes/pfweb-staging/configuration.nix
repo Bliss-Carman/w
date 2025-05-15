5e23c6ea117364b830d36e8a953e598be2c9f4cf
Dan Saul <dan@dsaul.ca>
2025-05-15 13:36:28 -0500

    .

--- nodes/pfweb-staging/configuration.nix
+++ nodes/pfweb-staging/configuration.nix
@@ -11,49 +11,19 @@
 	boot.loader.grub.device = "/dev/vda";
 	boot.loader.grub.useOSProber = true;
 
+	local.hardware.qemu.guest.enable = true;
+	local.services.reverse-proxy.enable = true;
+	local.services.httpd."www.staging.prairiefire.ca".enable = true;
+	local.system.runtimes.docker.enable = true;
+
+	# May have to be disabled in bootstrap.
+	local.services.wireguard-server.enable = true; 
+
 	networking.hostName = "pfweb"; # Define your hostname.
-
-	# Enable networking
-	#networking.networkmanager.enable = true;
-
-	# Set your time zone.
-	time.timeZone = "America/Winnipeg";
-
-	# Select internationalisation properties.
-	i18n.defaultLocale = "en_CA.UTF-8";
-
-	# Configure keymap in X11
-	services.xserver.xkb = {
-		layout = "us";
-		variant = "";
-	};
-
-	# Define a user account. Don't forget to set a password with ‘passwd’.
-	users.users.dan = {
-		isNormalUser = true;
-		description = "Dan Saul";
-		extraGroups = [ "networkmanager" "wheel" ];
-		packages = with pkgs; [];
-	};
-
-	# Allow unfree packages
-	nixpkgs.config.allowUnfree = true;
-
-	# List packages installed in system profile. To search, run:
-	# $ nix search wget
+	
 	environment.systemPackages = with pkgs; [
 
 	];
-	# Enable the OpenSSH daemon.
-	services.openssh = {
-		enable = true;
-		settings.PermitRootLogin = "yes";
-	};
-
-	local.services.reverse-proxy.enable = true;
-	local.services.wireguard-server.enable = true; 
-
-
-
+	
 	system.stateVersion = "24.11";
 }
