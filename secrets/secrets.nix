90453f30a5144d228b3eefeee276522a59b6cdba
Dan <dan@dsaul.ca>
2025-08-26 10:11:07 -0500

    .

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -46,7 +46,7 @@ in
 	"wgKey-carolyn-remote-private.age".publicKeys = users-admin;
 	"wgKey-tetrodesign-public.age".publicKeys = systems-all ++ users-admin;
 	"wgKey-tetrodesign-private.age".publicKeys = users-admin;
-	"wgKey-subscriptionmachine-public.age".publicKeys = users-admin;
+	"wgKey-subscriptionmachine-public.age".publicKeys = systems-all ++ users-admin;
 	"wgKey-subscriptionmachine-private.age".publicKeys = users-admin;
 	"dns-linode.age".publicKeys = systems-all ++ users-admin;
 	"mysql-root-password.age".publicKeys = systems-all ++ users-admin ++ user-dan;
