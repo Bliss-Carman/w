d66550be738c46d333e2175a26d2c718822012b3
Dan Saul <dan@dsaul.ca>
2026-03-22 14:20:20 -0500

    .

--- includes/stacks/stack-prairiefire.ca/stack-prairiefire.ca.nix
+++ includes/stacks/stack-prairiefire.ca/stack-prairiefire.ca.nix
@@ -17,6 +17,44 @@ in
 	config = lib.mkIf config.local.stacks."prairiefire.ca".enable {
 		local.services.httpd."www.prairiefire.ca".enable = lib.mkForce true;
 		
+
+		# REMOTE BACKUP
+		#systemctl list-timers
+		#systemctl start stack-prairiefire-s3-backup
+		local.backup.secrets.enable = lib.mkForce true;
+		age.secrets."bsackup-s3cfg-prairiefire.ca.age".file = ../../../secrets/backup-s3cfg-prairiefire.ca.age;
+		systemd.services."${packageName}-s3-backup" = {
+			script = ''
+set -o errexit
+set -o nounset
+set -o pipefail
+${pkgs.gnutar}/bin/tar cvf - --use-compress-program=${pkgs.xz}/bin/xz \
+	${config.local.stacks.dataRootPath}/${packageName} | \
+	${pkgs.age}/bin/age --recipients-file ${config.age.secrets."backup-encrypted-recipients.age".path} | \
+	${pkgs.s3cmd}/bin/s3cmd --verbose --config=${config.age.secrets."backup-s3cfg-prairiefire.ca.age".path} put - \
+	s3://dsaul-backup/${config.networking.hostName}-${packageName}.tar.xz.age || true
+  '';
+			serviceConfig = {
+				Type = "oneshot";
+				User = "root";
+				Nice = 19;
+				IOSchedulingClass = "idle";
+				IOSchedulingPriority=7;
+			};
+		};
+
+		systemd.timers."${packageName}-s3-backup" = {
+			wantedBy = [ "timers.target" ];
+			timerConfig = {
+				OnCalendar = "Mon *-*-* 01:00:00 America/Winnipeg";
+				Unit = "${packageName}-s3-backup.service";
+				Persistent = true;
+			};
+		};
+
+		# DOCKER
+
+
 		age.secrets."prairiefire.ca-env.age".file = ../../../secrets/prairiefire.ca-env.age;
 		age.secrets."mysql-root-password.age".file = ../../../secrets/mysql-root-password.age;
 		
